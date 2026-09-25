98c212e4cd90452f8de605578f72a082f2751a4c
watana256 <26051910@d.anabuki-college.net>
2026-09-25 16:21:12 +0900

    a

--- Project/GameProject/Game/Player.cpp
+++ Project/GameProject/Game/Player.cpp
@@ -1,1 +1,71 @@
 #include "Player.h"
+
+static TexAnim _Walk_right[] =
+{
+	{2,2},
+};
+static TexAnim _Walk_left[] =
+{
+	{1,1},
+};
+static TexAnim _Walk_up[] =
+{
+	{1,1},
+};
+static TexAnim _Walk_down[] =
+{
+	{1,1},
+};
+static TexAnim _damage_right[] =
+{
+	{1,1},
+};
+static TexAnim _damage_left[] =
+{
+	{1,1},
+};
+static TexAnim _damage_up[] =
+{
+	{1,1},
+};
+static TexAnim _damage_down[] =
+{
+	{1,1},
+};
+static TexAnim _death[] =
+{
+	{1,1},
+};
+
+TexAnimData Player::_anim_data[] =
+{
+	ANIMDATA(_Walk_right),
+	ANIMDATA(_Walk_left),
+	ANIMDATA(_Walk_up),
+	ANIMDATA(_Walk_down),
+	ANIMDATA(_damage_right),
+	ANIMDATA(_damage_left),
+	ANIMDATA(_damage_up),
+	ANIMDATA(_damage_down),
+	ANIMDATA(_death),
+};
+Player::Player(const CVector2D& pos) 
+	:Base(eType_Player)
+{
+	m_img = COPY_RESOURCE("Player", CImage);
+	m_pos_old = m_pos = pos;
+	m_img.SetSize(80, 80);
+	m_img.SetCenter(40, 40);
+	m_img.ChangeAnimation(eState_damage_up);
+}
+void Player::Update()
+{
+	m_img.UpdateAnimation();
+	m_pos_old = m_pos;
+}
+void Player::Draw()
+{
+	m_img.SetPos(m_pos);
+	m_img.Draw();
+	m_img.SetRect(128, 0, 192, 64);
+}
--- Project/GameProject/Game/Player.h
+++ Project/GameProject/Game/Player.h
@@ -4,11 +4,34 @@
 class Player :public Base
 {
 private:
+	enum {
+		eState_Walk_right,
+		eState_Walk_left,
+		eState_Walk_up,
+		eState_Walk_down,
+		eState_damage_right,
+		eState_damage_left,
+		eState_damage_up,
+		eState_damage_down,
+		eState_death
+	};
+	CImage m_img;
 
-
+	enum {
+		eAnim_Walk_right,
+		eAnim_Walk_left,
+		eAnim_Walk_up,
+		eAnim_Walk_down,
+		eAnim_damage_right,
+		eAnim_damage_left,
+		eAnim_damage_up,
+		eAnim_damage_down,
+		eAnim_death
+	};
 public:
 	Player(const CVector2D& pos);
 	static TexAnimData _anim_data[];
 	void Update();
 	void Draw();
+	void TakeDamage(int damage);
 };
